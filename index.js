cf218a74ef890e8775017d786ea4e67d4726b7a3
Liam Taylor <liam.taylor888@googlemail.com>
2016-11-10 08:27:51 +0000

    1) Renamed NoteController prototype method 2) Added method to update NoteListView HTML when new Notes are added to NoteList 3) Minor refactor

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 
 var noteList = new NoteList;
+
 noteList.addNote("Favourite drink: seltzer");
 noteList.addNote("Favourite animal: Meercat");
+
 var noteListController = new NoteListController(noteList);
 
-
-
-
-noteListController.insertNoteViewHTML("app", noteListController.noteListHTML);
-
-
-noteListController.updateURL()
+noteList.addNote("Favourite food: pizza");
+noteListController.updateHTMLToRender();
+noteListController.renderHTMLToBrowser("app", noteListController.noteListHTML);
+noteListController.triggerChangeToScreen()
 
 // noteListController.returnSingleNote();
--- note-controller.js
+++ note-controller.js
@@ -5,20 +5,22 @@
     this.noteList = noteList;
     this.noteListView = new NoteListView(noteList);
     this.noteListHTML = this.noteListView.convertNotesToHTML()
-    console.log(this.noteListHTML)
   };
 
-  NoteListController.prototype.insertNoteViewHTML = function(element, text) {
+  NoteListController.prototype.renderHTMLToBrowser = function(element, text) {
     document.getElementById(element).innerHTML = text;
   };
 
-  NoteListController.prototype.updateURL= function() {
+  NoteListController.prototype.updateHTMLToRender= function() {
+    this.noteListHTML = this.noteListView.convertNotesToHTML();
+  };
+
+  NoteListController.prototype.triggerChangeToScreen= function() {
     window.addEventListener("hashchange", this.setsUpNoteAndId());
   };
 
     NoteListController.prototype.setsUpNoteAndId = function() {
-      console.log("hawwo");
-      this.returnSingleNote(this.getNoteIdFromUrl(window.location));
+      this.createSingleNoteViewFromNoteList(this.getNoteIdFromUrl(window.location));
     };
 
   NoteListController.prototype.testFunction = function(){
@@ -27,79 +29,20 @@
 
   NoteListController.prototype.getNoteIdFromUrl = function(location) {
     return location.hash.split("#")[1];
-    console.log("haww")
     console.log(location.hash.split("#")[1])
-
   };
 
-
-  NoteListController.prototype.returnSingleNote = function(id) {
-    console.log(id)
-    console.log(this.noteList)
-    var abba = new SingleNoteView(noteList.listOfNotes[id]);
-    // console.log(singleNoteView)
-    // console.log(singleNoteView.createNoteView())
-    abba.createNoteView();
-    console.log(abba)
-    var fab = abba.noteView
-    this.insertNoteViewHTML("app", fab)
+  NoteListController.prototype.createSingleNoteViewFromNoteList = function(id) {
+    var singleNoteView = new SingleNoteView(noteList.listOfNotes[id]);
+    singleNoteView.createNoteView();
+    var contentToRender = singleNoteView.noteView
+    this.renderHTMLToBrowser("app", contentToRender)
   };
 
   exports.NoteListController = NoteListController;
 
 })(this);
-//
-// var noteList = new NoteList;
-// var noteListController = new NoteListController(noteList);
-//
-// noteList.addNote("Favourite drink: seltzer");
-// noteList.addNote("Favourite animal: Meercat");
-// console.log(note)
-//
-// noteListController.insertNoteViewHTML();
-//
-//
-// function updateURL() {
-//     window.addEventListener("hashchange", setsUpNoteAndId);
-//   };
-//
-//   function testFunction(){
-//     console.log("It's working")
-//   };
-//
-//   function setsUpNoteAndId() {
-//       returnSingleNote(getNoteIdFromUrl(window.location));
-//     };
-//
-//   function getNoteIdFromUrl(location) {
-//       return location.hash.split("#")[1];
-//     };
-//
-//     function returnSingleNote(id) {
-//       console.log(id)
-//       console.log(noteList)
-//     singleNoteView = new SingleNoteView(noteList.listOfNotes[id]);
-//      document
-//        .getElementById("app")
-//        .innerHTML = singleNoteView.createNoteView();
-//    };
-//
-//    updateURL()
 
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//
 // var element = document.getElementById("app").innerHTML;
 //
 // function changeHTMLText(id, string) {
